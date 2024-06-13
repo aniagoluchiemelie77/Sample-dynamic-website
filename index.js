5e03191ba6735c1a8aeee04541b6b59c9a9a6938
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-06-13 21:44:47 +0100

    foundation building

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+const section2 = document.querySelector('.section2');
+const header = document.querySelector('.header');
+const initCoords = section2.getBoundingClientRect();
+console.log(initCoords);
+
+
+window.addEventListener('scroll', function (e) {
+    if(this.window.scrollY > initCoords.top){
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove('sticky');
+    }
+});

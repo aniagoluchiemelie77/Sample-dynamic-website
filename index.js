7144ab0503875d2423338773d743136afc9775cb
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-06-18 15:39:12 +0100

    added :root{ }

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 const section2 = document.querySelector('.section2');
 const menubtn = document.querySelector('.mainheader__header-nav-1');
+const menubtn2 = document.getElementById('searchicon');
+const headerSearchBar = document.querySelector('.mainheader__searchbox-container');
 const closeMenuBtn = document.querySelector('.sidebarbtn');
 const header = document.querySelector('.header');
 const initCoords = section2.getBoundingClientRect();
 const sidebar = document.getElementById('sidebar');
+let surroundingDivs = !sidebar;
 console.log(initCoords);
 
 
@@ -16,14 +19,22 @@ window.addEventListener('scroll', function (e) {
     }
 });
 
-//displaying menu bar
+//displaying sidebar
 const removeHiddenClass = function (e) {
     e.stopPropagation();
     sidebar.classList.remove('hidden');
 }
 menubtn.addEventListener('click', removeHiddenClass);
+//closing sidebar
 const addHiddenClass = function (e) {
     e.stopPropagation();
     sidebar.classList.toggle('hidden');
 }
 closeMenuBtn.addEventListener('click', addHiddenClass);
+//displaying search bar
+const removeHiddenClass2 = function (e) {
+    e.stopPropagation();
+    headerSearchBar.classList.toggle('hidden');
+    menubtn2.classList.toggle('hidden');
+}
+menubtn2.addEventListener('click', removeHiddenClass2);

ae1fe307a3f21954407d5d6365b83192224bd6d1
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-07-14 21:11:56 +0100

    foundation programming

--- includes/section1.php
+++ includes/section1.php
@@ -1,2 +1,41 @@
 <?php
+echo"hullo";
 ?>
+<section class="section1">
+        <div class="section1__div1 larger__div">
+            <article>
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="larger__div__subdiv">
+                    <h1>Sample Niche</h1>
+                    <h2>Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p>June 13, 2024</p>
+                </div>
+            </article>
+        </div>
+        <div class="section1__div2 smallerdivs">
+            <article class="section1__articlearticle2 smaller__div">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="smaller__div__subdiv">
+                    <h1>Sample Niche</h1>
+                    <h2>Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p>June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section1__article__div3 smaller__div border-gradient-top">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="smaller__div__subdiv">
+                    <h1>Sample Niche</h1>
+                    <h2>Microsoft, late to the game on dangerous DNSSEC Zero-day flaw.</h2>
+                    <p>June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section1__articlearticle3 smaller__div border-gradient-top">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="smaller__div__subdiv">
+                    <h1>Sample Niche</h1>
+                    <h2>Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p>June 13, 2024</p>
+                </div>
+            </article>
+        </div>
+    </section>
---
+++ includes/section3.php
@@ -0,0 +1,55 @@
+<?php
+?>
+<section class="section3">
+        <div class="section3__div1">
+            <h1>Press Releases</h1>
+            <a href="pressrelease.html" class="section2__div2__link">See more
+                <svg width="2.5rem" height="2.5rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="24" height="24" fill="#eee"/>
+                    <path d="M9.5 7L14.5 12L9.5 17" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </a>
+        </div>
+        <div class="section3__div2">
+            <article class="section3__div2__article1">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="section3__subdiv">
+                    <h1 class="section3__subdiv-h1">Sample Niche</h1>
+                    <h2 class="section3__subdiv-h2">Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p class="section3__subdiv-p">June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section3__div2__article2 border-gradient-side-dark">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="section3__subdiv">
+                    <h1 class="section3__subdiv-h1">Sample Niche</h1>
+                    <h2 class="section3__subdiv-h2">Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p class="section3__subdiv-p">June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section3__div2__article3 border-gradient-side-dark">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="section3__subdiv">
+                    <h1 class="section3__subdiv-h1">Sample Niche</h1>
+                    <h2 class="section3__subdiv-h2">Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p class="section3__subdiv-p">June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section3__div2__article4 border-gradient-side-dark">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="section3__subdiv">
+                    <h1 class="section3__subdiv-h1">Sample Niche</h1>
+                    <h2 class="section3__subdiv-h2">Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p class="section3__subdiv-p">June 13, 2024</p>
+                </div>
+            </article>
+            <article class="section3__div2__article5 border-gradient-side-dark">
+                <img src="images\image1.jpeg" alt="article image">
+                <div class="section3__subdiv">
+                    <h1 class="section3__subdiv-h1">Sample Niche</h1>
+                    <h2 class="section3__subdiv-h2">Microsoft, Late to the Game on Dangerous DNSSEC Zero-day flaw.</h2>
+                    <p class="section3__subdiv-p">June 13, 2024</p>
+                </div>
+            </article>
+        </div>
+    </section>

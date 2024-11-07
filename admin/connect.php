87b1142e5dbb69344bffef6711e71ce3da33f686
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-11-07 17:36:53 +0100

    latest commit

--- admin/connect.php
+++ admin/connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "new_posts";
+$db = "blog";
 $conn = new mysqli($host, $user, $pass, $db);
 if($conn->connect_error){
     echo "Failed to connect".$conn->connect_error;

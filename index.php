e9b3183c6fee8dcd56eae0511b5a040239a18654
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2025-02-24 14:05:11 +0100

    still working on the styles

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 <html lang="en">
     <head>
 	    <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <meta name="description" content="Article website" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">

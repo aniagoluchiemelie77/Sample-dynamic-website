dc23ea47733ccf1b75ea5160c81aab16ad0b43c7
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2025-04-29 23:44:22 +0100

    Still on Translations

--- admin/create_new/message.php
+++ admin/create_new/message.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
-require ("../connect.php");
-include ('../crudoperations.php');
+require("../connect.php");
+include('../crudoperations.php');
+require("../init.php");
+$translationFile = "../translation_files/lang/{$language}.php";
+if (file_exists($translationFile)) {
+    include $translationFile;
+} else {
+    $translations = []; // Initialize as empty array to avoid undefined variable errors
+}
 $_SESSION['status_type'] = "";
 $_SESSION['status'] = "";
 $post_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
@@ -9,9 +16,10 @@ $user_type = $_GET['usertype'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content="Tech News and Articles website" />
     <meta name="keywords" content="Tech News, Content Writers, Content Strategy" />
@@ -19,125 +27,59 @@ $user_type = $_GET['usertype'];
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
-    <meta name="author" content="Aniagolu Diamaka"/>
+    <meta name="author" content="Aniagolu Diamaka" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../admin.css"/>
-	<title>Add Message</title>
+    <link rel="stylesheet" href="../admin.css" />
+    <title><?php echo $translations['send_message']; ?></title>
 </head>
+
 <body>
-    <?php 
-        require("../extras/header2.php");
-        if($post_id > 0){
-            if($user_type == "Editor"){
-                $geteditor_sql = "SELECT id, image, firstname FROM editor WHERE id = $post_id";
-                $geteditor_result = $conn->query($geteditor_sql);
-                if ($geteditor_result->num_rows > 0) {
-                    $row = $geteditor_result->fetch_assoc();
-                    echo "<div class='newpost_body2'>
+    <?php
+    require("../extras/header2.php");
+    if ($post_id > 0) {
+        if ($user_type == "Editor") {
+            $geteditor_sql = "SELECT id, image, firstname FROM editor WHERE id = $post_id";
+            $geteditor_result = $conn->query($geteditor_sql);
+            if ($geteditor_result->num_rows > 0) {
+                $row = $geteditor_result->fetch_assoc();
+                echo "<div class='newpost_body2'>
                             <div class='nav_quicklinks'>
-                                <a href = '../admin_homepage.php'>Home</a> > <p>Send Message to <span>".$row['firstname']."</span> </p>
+                                <a href = '../admin_homepage.php'>$translations[home]</a> > <p>$translations[send_message_i] <span>" . $row['firstname'] . "</span> </p>
                             </div>
                             <div class='message_div-container'>
                                 <div class='message_div-container_subdiv'>
-                                    <img src='../../".$row['image']."' alt='Editors Image'/>
+                                    <img src='../../" . $row['image'] . "' alt='Editors Image'/>
                                     <div class='message_div-container_subdiv-imagebody'>
                                         <form id='updateForm' action='message.php' method='POST'>
                                             <div class='input_group'>
-                                                <input name='subject' placeholder='Title..'/>
-                                                <p><span>* </span>Optional</p>
+                                                <input name='subject' placeholder='$translations[message_title]..'/>
+                                                <p><span>* </span>$translations[message_title_i]</p>
                                             </div>
                                             <div class='input_group'>
-                                                <label for='content'>Compose:</label>
+                                                <label for='content'>$translations[compose]:</label>
                                                 <textarea name='message'></textarea>
                                             </div>
                                             <input value='editor' style='display:none' name='user_type'/>
-                                            <input value='".$row['id']."' style='display:none' name='user_id'/>
-                                            <input type='submit' name='send_message' value='Send' class='btn send_messagebtn'/>
+                                            <input value='" . $row['id'] . "' style='display:none' name='user_id'/>
+                                            <input type='submit' name='send_message' value='$translations[send_message]' class='btn send_messagebtn'/>
                                         </form>
                                     </div>
                                 </div>
                             </div>
                         </div>";
-                }
-
-            }
-            if($user_type == "Writer"){
-                $geteditor_sql = "SELECT id, image, firstname FROM writer WHERE id = $post_id";
-                $geteditor_result = $conn->query($geteditor_sql);
-                if ($geteditor_result->num_rows > 0) {
-                    $row = $geteditor_result->fetch_assoc();
-                    echo "<div class='newpost_body2'>
-                            <div class='nav_quicklinks'>
-                                <a href = '../admin_homepage.php'>Home</a> > <p>Send Message to <span>".$row['firstname']."</span> </p>
-                            </div>
-                            <div class='message_div-container'>
-                                <div class='message_div-container_subdiv'>
-                                    <img src='../../".$row['image']."' alt='Writers Image'/>
-                                    <div class='message_div-container_subdiv-imagebody'>
-                                        <form action='' method='post'>
-                                            <div class='input_group'>
-                                                <input name='subject' placeholder='Title..'/>
-                                                <p><span>* </span>Optional</p>
-                                            </div>
-                                            <div class='input_group'>
-                                                <label for='content'>Compose:</label>
-                                                <textarea name='message'></textarea>
-                                            </div>
-                                            <input value='writer' style='display:none' name='user_type'/>
-                                            <input value='".$row['id']."' style='display:none' name='user_id'/>
-                                            <input type='submit' name='send_message' value='Send' class='btn send_messagebtn'/>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>";
-                }
-
-            }
-            if($user_type == "OtherUser"){
-                $geteditor_sql = "SELECT id, image, firstname, role FROM otherwebsite_users WHERE id = $post_id";
-                $geteditor_result = $conn->query($geteditor_sql);
-                if ($geteditor_result->num_rows > 0) {
-                    $row = $geteditor_result->fetch_assoc();
-                    echo "<div class='newpost_body2'>
-                            <div class='nav_quicklinks'>
-                                <a href = '../admin_homepage.php'>Home</a> > <p>Send Message to <span>".$row['firstname']."</span> </p>
-                            </div>
-                            <div class='message_div-container'>
-                                <div class='message_div-container_subdiv'>
-                                    <img src='../../".$row['image']."' alt='Writers Image'/>
-                                    <div class='message_div-container_subdiv-imagebody'>
-                                        <form action='' method='post'>
-                                            <div class='input_group'>
-                                                <input name='subject' placeholder='Title..'/>
-                                                <p><span>* </span>Optional</p>
-                                            </div>
-                                            <div class='input_group'>
-                                                <label for='content'>Compose:</label>
-                                                <textarea name='message'></textarea>
-                                            </div>
-                                            <input value='otherwebsite_users' style='display:none' name='user_type'/>
-                                            <input value='".$row['id']."' style='display:none' name='user_id'/>
-                                            <input type='submit' name='send_message' value='Send' class='btn send_messagebtn'/>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>";
-                }
-
             }
         }
+    }
     ?>
     <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $user_id = $_POST['user_id'];
         $user_type = $_POST['user_type'];
         $message = $_POST['message'];
         $title = $_POST['subject'];
         $date = date('y-m-d');
         date_default_timezone_set('Africa/Lagos');
-        $time = date('H:i:s'); 
+        $time = date('H:i:s');
         $stmt = $conn->prepare("INSERT INTO messages (user_id, user_type, title, message, Date, time) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("isssss", $user_id, $user_type, $title, $message, $date, $time);
         if ($stmt->execute()) {
@@ -148,13 +90,14 @@ $user_type = $_GET['usertype'];
             $_SESSION['status'] = "Post Published Successfully!";
         }
         $stmt->close();
-        }
+    }
     ?>
     <script src="../admin.js"></script>
     <script type="text/javascript" src="https://cdn.tiny.cloud/1/mshrla4r3p3tt6dmx5hu0qocnq1fowwxrzdjjuzh49djvu2p/tinymce/6/tinymce.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="sweetalert2.all.min.js"></script>
 </body>
+
 </html>
 <?php
 /*
--- admin/messages.php
+++ admin/messages.php
@@ -1,118 +1,128 @@
 <?php
 session_start();
-include("../connect.php");
+include("connect.php");
+require("init.php");
+$translationFile = "translation_files/lang/{$language}.php";
+if (file_exists($translationFile)) {
+    include $translationFile;
+} else {
+    $translations = []; // Initialize as empty array to avoid undefined variable errors
+}
 $usertype = " ";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content="Tech News and Articles website" />
     <meta name="keywords" content="Tech News, Content Writers, Content Strategy" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
-    <meta name="author" content="Aniagolu Diamaka"/>
+    <meta name="author" content="Aniagolu Diamaka" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="admin.css"/>
-	<title>Add Message</title>
+    <link rel="stylesheet" href="admin.css" />
+    <title><?php echo $translations['add_message']; ?></title>
 </head>
+
 <body>
-    <?php require("extras/header.php");?>
+    <?php require("extras/header.php"); ?>
     <div class="users">
         <div class="page_links">
-            <a href="admin_homepage.php">Home</a> > <p>Send Messages</p>
+            <a href="admin_homepage.php"><?php echo $translations['home']; ?></a> > <p><?php echo $translations['send_messages']; ?></p>
         </div>
         <div class="users_editor_div userdiv">
             <div class="user_header">
-                <h2>Editors</h2>
+                <h2><?php echo $translations['editors']; ?></h2>
             </div>
             <div class="users_div_subdiv border-gradient-side-dark">
-            <?php
+                <?php
                 $selectalleditors = "SELECT id, image, firstname, lastname FROM editor ORDER BY id DESC";
                 $selectalleditors_result = $conn->query($selectalleditors);
                 if ($selectalleditors_result->num_rows > 0) {
                     $sn = 0;
-                    while($row = $selectalleditors_result->fetch_assoc()) {
-                        $sn++; 
+                    while ($row = $selectalleditors_result->fetch_assoc()) {
+                        $sn++;
                         $usertype = "Editor";
-                        echo "<div class='users_div_subdiv_subdiv divimages' style=background-image:url('../".$row['image']."')>
+                        echo "<div class='users_div_subdiv_subdiv divimages' style=background-image:url('../" . $row['image'] . "')>
                                     <div class='divimages_side--back'>
-                                        <p class='users_div_subdiv_p'><span>".$row['firstname']."</span></p>
-                                        <p class='users_div_subdiv_p'><span>".$row['lastname']."</span></p> 
+                                        <p class='users_div_subdiv_p'><span>" . $row['firstname'] . "</span></p>
+                                        <p class='users_div_subdiv_p'><span>" . $row['lastname'] . "</span></p> 
                                         <center>
                                             <div class='users_delete_edit'>
-                                                <a class='users_edit' href='create_new/message.php?usertype=$usertype&id=".$row['id']."'>Message</a>
+                                                <a class='users_edit' href='create_new/message.php?usertype=$usertype&id=" . $row['id'] . "'>$translations[send_message]</a>
                                             </div>
                                         </center>
                                     </div>
-                        </div>";                         
+                        </div>";
                     };
                 };
-            ?>
-            </div>
-        </div>
-        <div class="users_writer_div userdiv">
-            <div class="user_header">
-                <h2>Writers</h2>
-            </div>
-            <div class="users_div_subdiv border-gradient-side-dark">
-                <?php
-                    $selectallwriters = "SELECT id, image, firstname, lastname FROM writer ORDER BY id DESC ";
-                    $selectallwriters_result = $conn->query($selectallwriters);
-                    if ($selectallwriters_result->num_rows > 0) {
-                        $sn = 0;
-                        while($row = $selectallwriters_result->fetch_assoc()) {
-                            $sn++; 
-                            echo "<div class='users_div_subdiv_subdiv divimages'style=background-image:url('../".$row['image'].">
-                                    <div class='divimages_side--back'>
-                                        <p class='users_div_subdiv_p'><span>".$row['firstname']."</span></p>
-                                        <p class='users_div_subdiv_p'><span>".$row['lastname']."</span></p> 
-                                        <center>
-                                            <div class='users_delete_edit'>
-                                                <a class='users_edit' href='create_new/message.php?id=".$row['id']."&usertype=Writer'>Message</a>
-                                            </div>
-                                        </center>
-                                    </div>
-                            </div>";                         
-                        };
-                    };
                 ?>
             </div>
         </div>
         <div class="users_writer_div userdiv">
             <div class="user_header">
-                <h2>Others Users</h2>
+                <h2><?php echo $translations['writers']; ?></h2>
             </div>
             <div class="users_div_subdiv border-gradient-side-dark">
                 <?php
-                    $selectallotherusers = "SELECT id, image, firstname, lastname, role FROM otherwebsite_users ORDER BY id DESC";
-                            $selectallotherusers_result = $conn->query($selectallotherusers);
-                            if ($selectallotherusers_result->num_rows > 0) {
-                                $sn = 0;
-                                while($row = $selectallotherusers_result->fetch_assoc()) {
-                                    $sn++; 
-                                    echo "<div class='users_div_subdiv_subdiv divimages' style=background-image:url('../".$row['image'].">
+                $selectallwriters = "SELECT id, image, firstname, lastname, email FROM writer ORDER BY id DESC ";
+                $selectallwriters_result = $conn->query($selectallwriters);
+                if ($selectallwriters_result->num_rows > 0) {
+                    $sn = 0;
+                    while ($row = $selectallwriters_result->fetch_assoc()) {
+                        $sn++;
+                        echo "<div class='users_div_subdiv_subdiv divimages'style=background-image:url('../" . $row['image'] . ">
+                                    <div class='divimages_side--back'>
+                                        <p class='users_div_subdiv_p'><span>" . $row['firstname'] . "</span></p>
+                                        <p class='users_div_subdiv_p'><span>" . $row['lastname'] . "</span></p> 
+                                        <center>
+                                            <div class='users_delete_edit'>
+                                                <a class='users_edit' href='mailto:" . $row['email'] . "' target='_blank'>$translations[send_message]</a>
+                                            </div>
+                                        </center>
+                                    </div>
+                            </div>";
+                    };
+                };
+                ?>
+            </div>
+        </div>
+        <div class="users_writer_div userdiv">
+            <div class="user_header">
+                <h2><?php echo $translations['other_website_users']; ?></h2>
+            </div>
+            <div class="users_div_subdiv border-gradient-side-dark">
+                <?php
+                $selectallotherusers = "SELECT id, image, firstname, lastname, role, email  FROM otherwebsite_users ORDER BY id DESC";
+                $selectallotherusers_result = $conn->query($selectallotherusers);
+                if ($selectallotherusers_result->num_rows > 0) {
+                    $sn = 0;
+                    while ($row = $selectallotherusers_result->fetch_assoc()) {
+                        $sn++;
+                        echo "<div class='users_div_subdiv_subdiv divimages' style=background-image:url('../" . $row['image'] . ">
                                             <div class='divimages_side--back'>
-                                                <p class='users_div_subdiv_p'><span>".$row['firstname']."</span></p>
-                                                <p class='users_div_subdiv_p'><span>".$row['lastname']."</span></p> 
-                                                <p class='users_div_subdiv_p'><span>".$row['role']."</span></p> 
+                                                <p class='users_div_subdiv_p'><span>" . $row['firstname'] . "</span></p>
+                                                <p class='users_div_subdiv_p'><span>" . $row['lastname'] . "</span></p> 
+                                                <p class='users_div_subdiv_p'><span>" . $row['role'] . "</span></p> 
                                                 <center>
                                                     <div class='users_delete_edit'>
-                                                        <a class='users_edit' href='create_new/message.php?id=".$row['id']."&usertype=OtherUser'>Message</a>
+                                                        <a class='users_edit' href='mailto:" . $row['email'] . "' target='_blank'>$translations[send_message]</a>
                                                     </div>
                                                 </center>
                                             </div>
-                                    </div>";                         
-                                };
-                            };
-                        ?>
-                    </div>
-                </div>
+                                    </div>";
+                    };
+                };
+                ?>
             </div>
+        </div>
+    </div>
     <script type="text/javascript" src="https://cdn.tiny.cloud/1/mshrla4r3p3tt6dmx5hu0qocnq1fowwxrzdjjuzh49djvu2p/tinymce/6/tinymce.min.js"></script>
     <script src="admin.js"></script>
 </body>
+
 </html>
